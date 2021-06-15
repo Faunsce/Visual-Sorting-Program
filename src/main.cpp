14bf0ebdda06d98495cc6a37b3ed9d8cbc888f2e
Dominik Rutter <dominik.rutter@gmail.com>
2021-06-14 21:51:19 -0400

    Program init

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <vector>
+#include <random>
 
+const short int SET_RANGE = 1000;
 int main() {
-	std::cout << "Program \n";
-	std::cin.ignore();
+	std::vector<int> unsorted_vector;
+	for (int i = 0; i < SET_RANGE; i++) {
+		unsorted_vector.emplace_back(i);
+	}
+	for (const auto& index : unsorted_vector) {
+		std::cout << index << std::endl;
+	}
+	std::cin.ignore(1);
 }
